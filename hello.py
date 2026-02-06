034c8bddc85c227c8b74ffc4b14cac6eb143f63f
Youngrok Bahn <yrbahn@Youngroks-Mac-mini.local>
2026-02-06 10:17:43 +0900

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from Pi! This is a simple program created for GitHub.")
+    name = input("What's your name? ")
+    print(f"Nice to meet you, {name}!")
+
+if __name__ == "__main__":
+    main()
